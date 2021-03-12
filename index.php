db706aeb81cf60157d2791a0ec6217870f411383
Luke Hoang <lukemhoang@gmail.com>
2021-03-12 00:08:58 -0500

    add normalize css, google font, css, jquery, js

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>XDS | Dev team | Coding Challenge 1</title>
+
+    <!-- Google fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Habibi&display=swap" rel="stylesheet">
+
+    <!-- Normalize Css -->
+    <link rel="stylesheet" href="https://yarnpkg.com/en/package/normalize.css">
+
+    <!-- Internal Css -->
+    <link rel="stylesheet" href="./style.css?ver=<?=rand(0,10000)?>">
+
+    <!-- Jquery -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous"></script>
+
+    <!-- Internal Js -->
+    <script src="./main.js?ver=<?=rand(0,10000)?>"></script>
+</head>
+<body>
+    <h1>Hello world</h1>
+</body>
+</html>
